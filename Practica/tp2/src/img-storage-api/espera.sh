9197eded78a802f386fb1316792d7a03f533ce0c
poximan <poxi_man@yahoo.com>
2018-04-22 12:13:22 -0300

    avancces tp2

--- Practica/tp2/src/img-storage-api/espera.sh
+++ Practica/tp2/src/img-storage-api/espera.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#set -e # sale al primer error
+set -e # sale al primer error
 echo "Intentando conectar a $SERVIDOR:$PUERTO"
 while :
 do
   netcat -z $SERVIDOR $PUERTO && break
-  #sleep 1
+  sleep 1
 done
 #$COMANDO
