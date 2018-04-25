21f5635e25e2e45f2d8e210a7d6cea4ef5630daa
poximan <poxi_man@yahoo.com>
2018-04-24 23:36:50 -0300

    avances tp2

--- Practica/tp2/src/getInitMosquito.bat
+++ Practica/tp2/src/getInitMosquito.bat
@@ -1,5 +1,4 @@
 docker run -d --name cont_tmp --rm eclipse-mosquitto
-docker cp cont_tmp:/etc/mosquitto ./img-mosquitto/etc-mosq
-docker cp cont_tmp:/mosquitto ./img-mosquitto/root-mosq
+docker cp cont_tmp:/mosquitto ./img-mosquitto/
 
 docker stop cont_tmp
