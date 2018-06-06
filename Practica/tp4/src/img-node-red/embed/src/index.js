2bd09d63b4ebe9a0d21d78fd1456ce9c13fbf6da
poximan <poxi_man@yahoo.com>
2018-06-06 20:58:29 -0300

    avances tp4

--- Practica/tp4/src/img-node-red/embed/src/index.js
+++ Practica/tp4/src/img-node-red/embed/src/index.js
@@ -7,10 +7,10 @@ const resolve = require('path').resolve
 var app = express();
 
 // Add a simple route for static content served from 'public'
-app.use("/red", express.static("public"));
+//app.use("/red", express.static("public"));
 app.use("/red/mqtt", express.static("public"));
 
-app.get('/red/mqtt', function (req, res, next) {  
+app.get('/red/mqtt', function (req, res, next) {
   res.sendFile(resolve("index.html"), function (err) {
     if (err) {
       next(err);
---
+++ Practica/tp4/src/img-node-red/embed/src/public/cliente-mqtt.js
@@ -0,0 +1,76 @@
+/*
+----------- DEF CLIENTE ----------------
+*/
+const clientId = "clientId-" + parseInt(Math.random() * 100, 10).toString();
+console.log('Registrado como cliente: ' + clientId);
+var client = new Paho.MQTT.Client(window.location.host, 80, "/mqtt", clientId);
+
+/*
+----------- RUTAS ----------------
+*/
+const suscrip_nuevo = "/canvas/nuevo";
+const suscrip_editar = "/canvas/editar";
+const suscrip_editar_todas = suscrip_editar+"/#";
+
+/*
+----------- MENSAJES ----------------
+*/
+client.onConnectionLost = function (responseObject) {
+  console.log("[MQTT] - se perdió conexion: " + responseObject.errorMessage);
+}
+
+client.onMessageArrived = function (message) {
+
+  if(message.destinationName == suscrip_nuevo)
+    agregarGrafico(message.payloadString)
+
+  if(message.destinationName.startsWith(suscrip_editar))
+    editarAtributo(message.payloadString)
+};
+
+var options = {
+  onSuccess: () => {
+    console.log('[MQTT] - conectado');
+
+    client.subscribe(suscrip_nuevo, {
+      onSuccess: () => {
+        console.log('[MQTT] - suscripto a ' + suscrip_nuevo);
+      },
+      onFailure: () => {
+        console.log('[MQTT] - no se pudo realizar suscripcion a ' + suscrip_nuevo);
+      }
+    })
+
+    client.subscribe(suscrip_editar_todas, {
+      onSuccess: () => {
+        console.log('[MQTT] - suscripto a ' + suscrip_editar_todas);
+      },
+      onFailure: () => {
+        console.log('[MQTT] - no se pudo realizar suscripcion a ' + suscrip_editar_todas);
+      }
+    })
+
+  },
+  onFailure: () => {
+    console.log('[MQTT] - no se pudo conectar');
+  }
+};
+
+/*
+----------- FUNCIONES ----------------
+*/
+function agregarGrafico(grafico) {
+  $("#form-archiv").append(grafico);
+}
+
+function editarAtributo(atributo) {
+  console.log("aca se edita grafico");
+  console.log(atributo);
+
+  console.log(document.getElementById("form-archiv").innerHTML);
+}
+
+/*
+----------- CONECTAR ----------------
+*/
+client.connect(options);
