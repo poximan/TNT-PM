106ca162fa1dc6dd7e89d25ee31adb34a9f7706f
poximan <poxi_man@yahoo.com>
2018-06-17 22:36:08 -0300

    agregando modificacion de nivel en ollas

--- Practica/tp4/src/img-node-red/embed/src/public/cliente-mqtt.js
+++ Practica/tp4/src/img-node-red/embed/src/public/cliente-mqtt.js
@@ -6,16 +6,28 @@ console.log('Registrado como cliente: ' + clientId);
 var client = new Paho.MQTT.Client(window.location.host, 80, "/mqtt", clientId);
 
 /*
+----------- BASE PARA CALCULAR COMPLEMENTO EN NIVELES ----------------
+*/
+let selection = d3.select("svg").select("#silo_malta_nivel");
+let silo_malta_nivel_piso = parseFloat(selection.attr("y")) + parseFloat(selection.attr("height"));
+
+selection = d3.select("svg").select("#mezclado_malta_nivel");
+let mezclado_malta_nivel_piso = parseFloat(selection.attr("y")) + parseFloat(selection.attr("height"));
+
+
+/*
 ----------- RUTAS ----------------
 */
-const suscrip_nuevo = "/canvas/nuevo";
-const suscrip_nuevo_todas = suscrip_nuevo+"/#";
-const suscrip_editar = "/canvas/editar";
-const suscrip_editar_todas = suscrip_editar+"/#";
-const suscrip_coccion_tambor_presion = "/pm/prod-cerveza/coccion/tambor/presion"
+const ub_geo_pm = "/pm"
+const proc_fab_cerv = ub_geo_pm+"/prod-cerveza"
 
-const nuevo_rectangulo = "/rect"
-const nuevo_circulo = "/circle"
+const sproc_silo_grano = proc_fab_cerv+"/silo-grano"
+const sproc_mezclado_malta = proc_fab_cerv+"/mezclado-malta"
+const sproc_precoccion = proc_fab_cerv+"/precoccion"
+
+const tarea_silo_nivel = sproc_silo_grano+"/tambor/nivel"
+const tarea_mezclado_nivel = sproc_mezclado_malta+"/tambor/nivel"
+const tarea_precoccion_nivel = sproc_precoccion+"/tambor/nivel"
 
 /*
 ----------- CONTENEDOR SVG ----------------
@@ -31,84 +43,57 @@ client.onConnectionLost = function (responseObject) {
 
 client.onMessageArrived = function (message) {
 
-  if(message.topic.startsWith(suscrip_nuevo))
-    agregarGrafico(message)
-  else
-    editarAtributo(message)
+  if(message.topic.startsWith(sproc_silo_grano)){
+    console.log("subproceso silo grano")
+    editarSiloGrano(message)
+  }
+
+  if(message.topic.startsWith(sproc_mezclado_malta)){
+    console.log("subproceso mezclado malta")
+    editarMezcladoMalta(message)
+  }
 };
 
 var options = {
   onSuccess: () => {
+
     console.log('conectado');
+    const pm_prod_cerv_todo = proc_fab_cerv+"/#"
 
-    client.subscribe(suscrip_nuevo_todas, {
-      onSuccess: () => { console.log('suscripto a ' + suscrip_nuevo_todas); },
-      onFailure: () => { console.log('no se pudo realizar suscripcion a ' + suscrip_nuevo_todas); }
-    })
-
-    client.subscribe(suscrip_editar_todas, {
-      onSuccess: () => { console.log('suscripto a ' + suscrip_editar_todas); },
-      onFailure: () => { console.log('no se pudo realizar suscripcion a ' + suscrip_editar_todas); }
-    })
-
-    client.subscribe(suscrip_coccion_tambor_presion, {
-      onSuccess: () => { console.log('suscripto a ' + suscrip_coccion_tambor_presion); },
-      onFailure: () => { console.log('no se pudo realizar suscripcion a ' + suscrip_coccion_tambor_presion); }
+    client.subscribe(pm_prod_cerv_todo, {
+      onSuccess: () => { console.log('suscripto a ' + pm_prod_cerv_todo); },
+      onFailure: () => { console.log('no se pudo realizar suscripcion a ' + pm_prod_cerv_todo); }
     })
   },
-  onFailure: () => { console.log('no se pudo conectar'); },
-  userName: "cerv",
-  password: "cerv"
+  onFailure: () => { console.log('no se pudo conectar'); }
 };
 
 /*
 ----------- FUNCIONES ----------------
 */
-function agregarGrafico(message) {
-
-  let sub_topico = message.topic.substring(suscrip_nuevo.length)
-  let grafico = message.payloadString;
-
-  var nuevo_grupo = svg_contenedor.append("g")
-      .attr("topico", sub_topico)
-
-  var nuevo_elemento = nuevo_grupo.selectAll(sub_topico.slice(1))
-      .data(eval(grafico))
-      .enter()
-      .append(sub_topico.slice(1));
-
-  if(sub_topico === nuevo_circulo){
-    nuevo_elemento
-    .attr("cx", function (d) { return d.cx; })
-    .attr("cy", function (d) { return d.cy; })
-    .attr("r", function (d) { return d.r; })
-    .style("stroke", function (d) { return d.stroke; })
-    .style("stroke-width", function (d) { return d.stroke_width; })
-    .style("fill", function (d) { return d.fill; })
-  }
-
-  if(sub_topico === nuevo_rectangulo){
-    nuevo_elemento
-    .attr("x", function (d) { return d.x; })
-    .attr("y", function (d) { return d.y; })
-    .attr("height", function (d) { return d.height; })
-    .attr("width", function (d) { return d.width; })
-    .style("fill", function (d) { return d.fill; })
-  }
-
-  adaptarArea()
-}
-
-function editarAtributo(message) {
+function editarSiloGrano(message) {
 
   let selection = d3.select("svg").selectAll('[topico="' + message.topic + '"]');
   let params = JSON.parse(message.payloadString);
 
-  selection.attr("fill", escalar(params));
+  selection.attr("height", params);
+  selection.attr("y", silo_malta_nivel_piso - params);
 }
 
+function editarMezcladoMalta(message) {
+
+  let selection = d3.select("svg").selectAll('[topico="' + message.topic + '"]');
+  let params = JSON.parse(message.payloadString);
+
+  selection.attr("height", params);
+  selection.attr("y", mezclado_malta_nivel_piso - params);
+}
+
+/*
+----------- AUXILIARES ----------------
+*/
 function escalar(params) {
-  return rgb(parseInt(params) * (65535/5));
+  return rgb(parseInt(params) * (65535/10));
 }
 
 function rgb(num) {
@@ -122,20 +107,6 @@ function rgb(num) {
 
 function rgbToHex(r, g, b) {
   return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-}
-
-/*
------------ REDIMENSIONAR AREA SVG ----------------
-*/
-function adaptarArea() {
-  var svgs = document.getElementsByTagName("svg");
-
-  var svg = svgs[0],
-  box = svg.getBBox(),
-  //viewBox = [box.x, box.y, box.width, box.height].join(" ");
-  viewBox = [0, 0, 500, 500].join(" ");
-
-  svg.setAttribute("viewBox", viewBox);
 }
 
 /*
